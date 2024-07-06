d2f634dfdab8eb3d125f048bf875f20e08ef287e
shetu81 <shetusir@gmail,com>
2024-07-06 21:41:24 +0600

    first commit

---
+++ largest_Number.php
@@ -0,0 +1,19 @@
+<?php 
+   
+  
+$number1 = 4; 
+$number2 = 5; 
+$number3 = 6; 
+  
+  
+if ($number1 > $number2)
+  $max=$number1;
+else
+  $max=$number2; 
+
+ if ($max> $number3)
+  echo"The largest number is: $max\n";
+else
+    echo"The largest number is: $max\n"; 
+
+?>
